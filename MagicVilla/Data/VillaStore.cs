8ac0b0250273c899cec4d9ae7680cac86b74f661
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-08-17 00:40:08 +0400

    get individual villa

--- MagicVilla/Data/VillaStore.cs
+++ MagicVilla/Data/VillaStore.cs
@@ -1,6 +1,12 @@
-﻿namespace MagicVilla.Data;
+﻿using MagicVilla.Models.Dto;
+
+namespace MagicVilla.Data;
 
 public static class VillaStore
 {
-    
+    public static List<VillaDTO> villaList = new List<VillaDTO>()
+    {
+        new VillaDTO { Id = 1, Name = "Pool View" },
+        new VillaDTO { Id = 1, Name = "Beach View" }
+    };
 }
