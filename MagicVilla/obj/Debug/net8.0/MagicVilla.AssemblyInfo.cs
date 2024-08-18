467279fc1d504dbb2cd702c9c127366bd0d27b79
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-08-19 00:27:27 +0400

    status code

--- MagicVilla/obj/Debug/net8.0/MagicVilla.AssemblyInfo.cs
+++ MagicVilla/obj/Debug/net8.0/MagicVilla.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5235b823a7a9b8b0fc0529eed9f11e7643e521b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac0b0250273c899cec4d9ae7680cac86b74f661")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
