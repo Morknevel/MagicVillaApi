3f54530ba9c5645a2b224f623f4fdd6ac63b75b0
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-09-05 18:15:39 +0400

    repository cleanup

---
+++ MagicVilla/Repository/Repository.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq.Expressions;
+using MagicVilla.Data;
+using MagicVilla.Models;
+using MagicVilla.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _db;
+    internal DbSet<T> dbset;
+    public Repository(ApplicationDbContext db)
+    {
+        _db = db;
+        this.dbset = db.Set<T>();
+    }
+
+    public async Task CreateAsync(T entity)
+    {
+        await dbset.AddAsync(entity);
+        await SaveAsync();
+    }
+    
+
+    public async Task<T> GetAsync(Expression<Func<T,bool>> filter = null, bool tracked = true)
+    {
+        IQueryable<T> query = dbset;
+        if (!tracked)
+        {
+            query = query.AsNoTracking();
+        }
+        if (filter != null)
+        {
+            query = query.Where(filter);
+        }
+        return await query.FirstOrDefaultAsync();
+    }
+
+    public async Task<List<T>> GetAllAsync(Expression<Func<T,bool>>? filter = null)
+    {
+        IQueryable<T> query = dbset;
+
+        if (filter != null)
+        {
+            query = query.Where(filter);
+        }
+        return await query.ToListAsync();
+    }
+
+    public async Task RemoveAsync(T entity)
+    {
+        dbset.Remove(entity);
+        await SaveAsync();
+    }
+    
+    
+    public async Task SaveAsync()
+    {
+        await _db.SaveChangesAsync();
+    }
+}
--- MagicVilla/obj/Debug/net8.0/MagicVilla.AssemblyInfo.cs
+++ MagicVilla/obj/Debug/net8.0/MagicVilla.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2caca37485367cfb08e48804027ade7764524bae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7154a67fd474a7b4f5430df337220c38e016d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
