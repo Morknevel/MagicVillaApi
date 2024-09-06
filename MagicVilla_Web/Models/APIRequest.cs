4d27c6581473682f6a555b15cabd59d974b6a157
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-09-07 00:45:36 +0400

    Web project dtos  and api models

---
+++ MagicVilla_Web/Models/APIRequest.cs
@@ -0,0 +1,10 @@
+﻿using MagicVilla_Utility;
+
+namespace MagicVilla_Web.Models;
+
+public class APIRequest
+{
+    public SD.ApiType  ApiType { get; set; }
+    public string url { get; set; }
+    public Object Data { get; set; }
+}
