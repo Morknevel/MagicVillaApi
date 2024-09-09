cfde86778b256044c20a2e073f45d23b3b31d83f
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-09-10 00:22:40 +0400

    adding villa service

---
+++ MagicVilla_Web/Models/APIResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace MagicVilla_Web.Models;
+
+public class APIResponse
+{
+    public HttpStatusCode StatusCode { get; set; }
+    public bool IsSuccess { get; set; } = true;
+    public List<String> ErrorMessages { get; set; }
+    public Object  Result { get; set; }
+}
