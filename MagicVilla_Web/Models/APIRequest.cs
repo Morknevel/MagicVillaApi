1ce5628bdaa4dc88dd7a89a109f5d8d829c882b9
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-09-09 21:18:18 +0400

    Base Service for API

--- MagicVilla_Web/Models/APIRequest.cs
+++ MagicVilla_Web/Models/APIRequest.cs
@@ -5,6 +5,6 @@ namespace MagicVilla_Web.Models;
 public class APIRequest
 {
     public SD.ApiType  ApiType { get; set; }
-    public string url { get; set; }
+    public string Url { get; set; }
     public Object Data { get; set; }
 }
