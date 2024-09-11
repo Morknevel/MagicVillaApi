07917169ff82f4ad36f220b652d744336f3cda2e
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-09-12 00:03:36 +0400

    update and delete villa

--- MagicVilla_Utility/obj/Debug/net8.0/MagicVilla_Utility.AssemblyInfo.cs
+++ MagicVilla_Utility/obj/Debug/net8.0/MagicVilla_Utility.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_Utility")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af709d4e137ec9887495f4fa4d8ba520b534217f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b390f9f1aa854847610e99e2da903097ea598c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_Utility")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_Utility")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
