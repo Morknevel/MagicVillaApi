f8a1a969ee9ee913765ec6072ec27d6de8cfe587
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-09-13 16:30:31 +0400

    Villa Home page

--- MagicVilla_Web/obj/Debug/net8.0/MagicVilla_Web.AssemblyInfo.cs
+++ MagicVilla_Web/obj/Debug/net8.0/MagicVilla_Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0736c7a7906ab7360e6b63d630457a0a3b2ee0db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52d65a695d2382556e836f821b7338fe8605c192")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
